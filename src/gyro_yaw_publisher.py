cfbbce9a58b216b194344178c5e522b76b0861f4
weskerwork@gmail.com <weskerwork@gmail.com>
2023-04-20 15:13:41 +0800

    update

---
+++ src/gyro_yaw_publisher.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+
+import rospy
+import serial
+import math
+from std_msgs.msg import Float64, Empty
+from geometry_msgs.msg import Point, Twist
+
+ser = serial.Serial(
+    port='COM7',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+
+current_position = Point()
+current_yaw = 0
+
+def position_callback(data):
+    global current_position
+    current_position.x = data.x
+    current_position.y = data.y
+    rospy.loginfo("Received position: x = %f, y = %f", data.x, data.y)
+
+def home_callback(data):
+    global current_yaw
+    angle_to_origin = calculate_angle_to_origin(current_position)
+    rospy.loginfo("Angle to origin: %f degrees", angle_to_origin)
+
+    shortest_angle = calculate_shortest_angle(current_yaw, angle_to_origin)
+    turn_direction = decide_turn_direction(shortest_angle)
+    execute_turn(pub_cmd_vel, shortest_angle, turn_direction)
+
+    move_forward_to_origin(pub_cmd_vel)
+
+    turn_to_zero_degrees(pub_cmd_vel)
+
+    pub_home_done.publish(Empty())
+
+def calculate_angle_to_origin(position):
+    dx = position.x
+    dy = position.y
+    angle_rad = math.atan2(dy, dx)
+    angle_deg = math.degrees(angle_rad)
+    return angle_deg
+
+def calculate_shortest_angle(current_angle, target_angle):
+    diff = target_angle - current_angle
+    return ((diff + 180) % 360) - 180
+
+def decide_turn_direction(shortest_angle):
+    if shortest_angle > 0:
+        return 1  # Turn left
+    else:
+        return -1  # Turn right
+
+def execute_turn(pub_cmd_vel, shortest_angle, turn_direction, tolerance=5, angular_speed=0.1):
+    global current_yaw
+
+    target_yaw = current_yaw + shortest_angle
+    target_yaw = (target_yaw + 360) % 360
+
+    twist = Twist()
+    twist.angular.z = turn_direction * angular_speed
+
+    rospy.loginfo("Turning to face origin...")
+
+    while not rospy.is_shutdown():
+        angle_error = calculate_shortest_angle(current_yaw, target_yaw)
+
+        if abs(angle_error) <= tolerance:
+            break
+
+        pub_cmd_vel.publish(twist)
+        rospy.sleep(0.1)
+
+    twist.angular.z = 0
+    pub_cmd_vel.publish(twist)
+    rospy.loginfo("Turn completed.")
+
+def move_forward_to_origin(pub_cmd_vel, tolerance=0.1, linear_speed=0.1):
+    rospy.loginfo("Moving forward to origin...")
+
+    twist = Twist()
+    twist.linear.x = linear_speed
+
+    while not rospy.is_shutdown():
+        distance_to_origin = math.sqrt(current_position.x**2 + current_position.y**2)
+
+        if distance_to_origin <= tolerance:
+            break
+
+        pub_cmd_vel.publish(twist)
+        rospy.sleep(0.1)
+
+    twist.linear.x = 0
+    pub_cmd_vel.publish(twist)
+    rospy.loginfo("Reached origin.")
+
+def turn_to_zero_degrees(pub_cmd_vel, tolerance=5, angular_speed=0.1):
+    rospy.loginfo("Turning to face 0 degrees...")
+
+    shortest_angle = calculate_shortest_angle(current_yaw, 0)
+    turn_direction = decide_turn_direction(shortest_angle)
+
+    execute_turn(pub_cmd_vel, shortest_angle, turn_direction, tolerance, angular_speed)
+
+    rospy.loginfo("Facing 0 degrees.")
+
+def main():
+    global pub_cmd_vel, pub_home_done, current_yaw
+
+    rospy.init_node("gyro_yaw_publisher", anonymous=True)
+    rospy.Subscriber("/dog/position", Point, position_callback)
+    rospy.Subscriber("/dog/home", Empty, home_callback)
+
+    pub = rospy.Publisher("yaw_data", Float64, queue_size=1)
+    pub_cmd_vel = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+    pub_home_done = rospy.Publisher("/dog/homedone", Empty, queue_size=1)
+
+    rate = rospy.Rate(10)  # 10 Hz
+
+    while not rospy.is_shutdown():
+        data = str(ser.readline())
+        splitdata = data.split(' ')
+
+        if "version: 34688\\r\\n" in data:
+            try:
+                yaw = splitdata[5]
+                removeIndex = yaw.index('\\xa1')
+                yaw = math.floor(float(yaw[0:removeIndex]))
+                if yaw < 0:
+                    yaw += 360
+
+                current_yaw = yaw
+                pub.publish(yaw)  # Publish the yaw value
+
+            except Exception as e:
+                rospy.logerr("Error processing serial data: %s", str(e))
+
+        rate.sleep()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
--- src/osc_control.py
+++ src/osc_control.py
@@ -2,6 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Empty
 import sys
 import tty
 import termios
@@ -19,63 +20,81 @@ def getch():
     return ch
 
 def process_keyboard_input(pub):
+    global paused
     twist = Twist()
 
     while not rospy.is_shutdown():
-        ch = getch()
+        if not paused:
+            ch = getch()
 
-        twist.linear.x = 0.0
-        twist.linear.y = 0.0
-        twist.linear.z = 0.0
-        twist.angular.x = 0.0
-        twist.angular.y = 0.0
-        twist.angular.z = 0.0
+            twist.linear.x = 0.0
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = 0.0
 
-        if ch == 'q':
-            break
-        elif ch == 'w':
-            twist.linear.x = 0.5
-        elif ch == 's':
-            twist.linear.x = -0.5
-        elif ch == 'a':
-            twist.linear.y = 0.5
-        elif ch == 'd':
-            twist.linear.y = -0.5
-        elif ch == 'j':
-            twist.angular.z = 1.0
-        elif ch == 'l':
-            twist.angular.z = -1.0
+            if ch == 'q':
+                break
+            elif ch == 'w':
+                twist.linear.x = 0.5
+            elif ch == 's':
+                twist.linear.x = -0.5
+            elif ch == 'a':
+                twist.linear.y = 0.5
+            elif ch == 'd':
+                twist.linear.y = -0.5
+            elif ch == 'j':
+                twist.angular.z = 1.0
+            elif ch == 'l':
+                twist.angular.z = -1.0
 
-        pub.publish(twist)
+            pub.publish(twist)
+
+def homedone_callback(msg):
+    global paused
+    print('Received homedone message')
+    paused = False
+
+def handle_home_osc_message(unused_addr, args):
+    global home_pub, paused
+    paused = True
+    empty_msg = Empty()
+    print('Received osc command: home')
+    home_pub.publish(empty_msg)
 
 def handle_osc_message(unused_addr, args, value):
-    global pub
-    command = args
-    twist = Twist()
+    global pub, paused
+    if not paused:
+        command = args
+        twist = Twist()
 
-    if command == 'forward':
-        twist.linear.x = 0.5
-    elif command == 'backward':
-        twist.linear.x = -0.5
-    elif command == 'left':
-        twist.linear.y = 0.5
-    elif command == 'right':
-        twist.linear.y = -0.5
-    elif command == 'turn_left':
-        twist.angular.z = 1.0
-    elif command == 'turn_right':
-        twist.angular.z = -1.0
-    elif command == 'stand':
-        twist.angular.y = 0.0
-    elif command == 'sit':
-        twist.angular.y = -0.2
-    print('Received osc command: %s' %str(command))
-    pub.publish(twist)
+        if command == 'forward':
+            twist.linear.x = 0.5
+        elif command == 'backward':
+            twist.linear.x = -0.5
+        elif command == 'left':
+            twist.linear.y = 0.5
+        elif command == 'right':
+            twist.linear.y = -0.5
+        elif command == 'turn_left':
+            twist.angular.z = 1.0
+        elif command == 'turn_right':
+            twist.angular.z = -1.0
+        elif command == 'stand':
+            twist.angular.y = 0.0
+        elif command == 'sit':
+            twist.angular.y = -0.2
+        print('Received osc command: %s' %str(command))
+        pub.publish(twist)
 
 def main():
-    global pub
+    global pub, home_pub, paused
+    paused = False
+    home_pub = rospy.Publisher('dog/home', Empty, queue_size=1)
     rospy.init_node('keyboard_input_node', anonymous=True)
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    rospy.Subscriber('dog/homedone', Empty, homedone_callback)
 
     disp = dispatcher.Dispatcher()
     disp.map("/forward", handle_osc_message, (pub, 'forward'))
@@ -86,6 +105,7 @@ def main():
     disp.map("/turn_right", handle_osc_message, (pub, 'turn_right'))
     disp.map("/stand", handle_osc_message, (pub, 'stand'))
     disp.map("/sit", handle_osc_message, (pub, 'sit'))
+    disp.map("/home", handle_home_osc_message)
 
     server = osc_server.ThreadingOSCUDPServer(('192.168.50.100', 10000), disp)
 
@@ -95,7 +115,7 @@ def main():
     rospy.loginfo("Serving on %s:%s", server.server_address[0], server.server_address[1])
     server.serve_forever()
 
-global pub
+global pub,home_pub,paused
 
 if __name__ == '__main__':
     try:
