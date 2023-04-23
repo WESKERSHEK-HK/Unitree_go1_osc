dbb879a2fb6f8eaa4abf1d8364eab157accc3f07
weskerwork@gmail.com <weskerwork@gmail.com>
2023-04-23 23:09:06 +0800

    Update robot_move_test.py

--- src/robot_move_test.py
+++ src/robot_move_test.py
@@ -35,13 +35,89 @@ def stop_function(event):
 
     running = False
 
-    # Rest of the stop_function code remains unchanged
+    # Rotate to 0 degrees
+    target_yaw = 0
+    while abs(yaw - target_yaw) > 0.01:
+        print("Rotating to 0 degrees")
+        angular_speed = 0.2 if yaw < target_yaw else -0.2
+        cmd = Twist()
+        cmd.angular.z = angular_speed
+        pub_cmd_vel.publish(cmd)
+        rospy.sleep(0.1)
+
+    # Move near original Z position
+    target_z = original_position.z
+    while abs(position.z - target_z) > 50:
+        print("Moving to original Z position")
+        check_position()
+        linear_speed = 0.1 if position.z < target_z else -0.1
+        cmd = Twist()
+        cmd.linear.y = linear_speed
+        pub_cmd_vel.publish(cmd)
+        rospy.sleep(0.1)
+
+    # Move near original X position
+    target_x = original_position.x
+    while abs(position.x - target_x) > 50:
+        print("Moving to original X position")
+        check_position()
+        linear_speed = 0.1 if position.x < target_x else -0.1
+        cmd = Twist()
+        cmd.linear.x = linear_speed
+        pub_cmd_vel.publish(cmd)
+        rospy.sleep(0.1)
+
+    # Stop and wait for shutdown
+    print("Stop and wait for shutdown")
+    cmd = Twist()
+    pub_cmd_vel.publish(cmd)
 
 def performance_function():
     global pub_cmd_vel, position, running
 
     while running:
-        # Rest of the performance_function code remains unchanged
+        # Randomly select a movement
+        movement = random.choice(["rotate_left", "rotate_right", "move_left", "move_right", "move_forward", "move_backward"])
+
+        cmd = Twist()
+
+        # Check if the movement would exceed limits
+        if movement == "move_left" and position.z >= 1300:
+            continue
+        elif movement == "move_right" and position.z <= 1000:
+            continue
+        elif movement == "move_forward" and position.x >= 750:
+            continue
+        elif movement == "move_backward" and position.x <= 550:
+            continue
+
+        # Perform the movement for 1 second
+        if movement == "rotate_left":
+            cmd.angular.z = 0.2
+        elif movement == "rotate_right":
+            cmd.angular.z = -0.2
+        elif movement == "move_left":
+            cmd.linear.y = 0.1
+        elif movement == "move_right":
+            cmd.linear.y = -0.1
+        elif movement == "move_forward":
+            cmd.linear.x = 0.1
+        elif movement == "move_backward":
+            cmd.linear.x = -0.1
+
+        pub_cmd_vel.publish(cmd)
+        rospy.sleep(1)
+
+        # Stop the movement
+        cmd = Twist()
+        pub_cmd_vel.publish(cmd)
+
+        # Print the movement and position after movement
+        print("Movement: {}, Position: x={}, z={}".format(movement, position.x, position.z))
+
+        # Rest for a random time between 10 and 60 seconds
+        rest_time = random.uniform(10, 60)
+        rospy.sleep(rest_time)
 
 def main():
     global yaw, position, original_position, pub_cmd_vel, running
