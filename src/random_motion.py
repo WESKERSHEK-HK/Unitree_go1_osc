3c914d63fa17973e583b952533d6a11ab3dad9c5
weskerwork@gmail.com <weskerwork@gmail.com>
2023-04-26 15:26:47 +0800

    Update random_motion.py

--- src/random_motion.py
+++ src/random_motion.py
@@ -6,76 +6,77 @@ from std_msgs.msg import Header
 import time
 import random
 
+def random_motion(high_cmd_ros):
+    motions = ['tilt_forward', 'tilt_backward', 'tilt_left', 'tilt_right', 'sit', 'raise']
+    target_motion = random.choice(motions)
+    if target_motion == 'tilt_forward':
+        high_cmd_ros.euler[0] = -0.3
+        high_cmd_ros.bodyHeight = 0.0
+    elif target_motion == 'tilt_backward':
+        high_cmd_ros.euler[0] = 0.3
+        high_cmd_ros.bodyHeight = 0.0
+    elif target_motion == 'tilt_left':
+        high_cmd_ros.euler[1] = -0.2
+        high_cmd_ros.bodyHeight = 0.0
+    elif target_motion == 'tilt_right':
+        high_cmd_ros.euler[1] = 0.2
+        high_cmd_ros.bodyHeight = 0.0
+    elif target_motion == 'sit':
+        high_cmd_ros.bodyHeight = -0.2
+    elif target_motion == 'raise':
+        high_cmd_ros.bodyHeight = 0.1
+
+    print("Random Motion: {}".format(target_motion))
+    return high_cmd_ros
+
+
+def reset_motion(selected_motion):
+    high_cmd_ros = selected_motion
+    high_cmd_ros.euler[0] = 0.0
+    high_cmd_ros.euler[1] = 0.0
+    high_cmd_ros.bodyHeight = 0.0
+
+    return high_cmd_ros
+
 def main():
     rospy.init_node('random_motion', anonymous=True)
 
     pub = rospy.Publisher('high_cmd', HighCmd, queue_size=1000)
 
-    loop_rate = rospy.Rate(500)
+    loop_rate = rospy.Rate(1000)
 
     motiontime = 0
     high_cmd_ros = HighCmd()
 
-    motions = [
-        {
-            'description': "Tilting robot body forward",
-            'motion': lambda: setattr(high_cmd_ros, 'euler', (lambda x: x, lambda x: x, -0.3))
-        },
-        {
-            'description': "Tilting robot body backward",
-            'motion': lambda: setattr(high_cmd_ros, 'euler', (lambda x: x, lambda x: x, 0.3))
-        },
-        {
-            'description': "Tilting robot body left",
-            'motion': lambda: setattr(high_cmd_ros, 'euler', (lambda x: x, -0.2, lambda x: x))
-        },
-        {
-            'description': "Tilting robot body right",
-            'motion': lambda: setattr(high_cmd_ros, 'euler', (lambda x: x, 0.2, lambda x: x))
-        },
-        {
-            'description': "Lowering robot body",
-            'motion': lambda: setattr(high_cmd_ros, 'bodyHeight', -0.2)
-        },
-        {
-            'description': "Raising robot body",
-            'motion': lambda: setattr(high_cmd_ros, 'bodyHeight', 0.1)
-        }
-    ]
-
-    def reset_motion():
-        setattr(high_cmd_ros, 'euler', (0.0, 0.0, 0.0))
-        setattr(high_cmd_ros, 'bodyHeight', 0.0)
-
     while not rospy.is_shutdown():
 
-        # Select a random motion and execute it
-        selected_motion = random.choice(motions)
-        print(selected_motion['description'])
-        selected_motion['motion']()
-
         #high_cmd_ros.header.stamp = rospy.Time.now()
-        high_cmd_ros.levelFlag = 1
+        high_cmd_ros.head[0] = 0xFE
+        high_cmd_ros.head[1] = 0xEF
+        high_cmd_ros.levelFlag = 'HIGHLEVEL'
         high_cmd_ros.mode = 1
 
         # Reset other motion parameters
         high_cmd_ros.gaitType = 0
         high_cmd_ros.speedLevel = 0
         high_cmd_ros.footRaiseHeight = 0
-        high_cmd_ros.velocity.x = 0.0
-        high_cmd_ros.velocity.y = 0.0
+        high_cmd_ros.velocity[0]= 0.0
+        high_cmd_ros.velocity[1]= 0.0
         high_cmd_ros.yawSpeed = 0.0
         high_cmd_ros.reserve = 0
 
-        pub.publish(high_cmd_ros)
+        # Select a random motion and execute it
+        selected_motion = random_motion(high_cmd_ros)
+        
+        pub.publish(selected_motion)
         loop_rate.sleep()
 
         # Reset motion to neutral position
         print("Resetting motion to neutral position")
-        reset_motion()
+        ori_motion = reset_motion(selected_motion)
 
         #high_cmd_ros.header.stamp = rospy.Time.now()
-        pub.publish(high_cmd_ros)
+        pub.publish(ori_motion)
         loop_rate.sleep()
 
         # Delay between 10 to 30 seconds
