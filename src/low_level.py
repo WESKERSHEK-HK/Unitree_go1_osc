10f9d552fed7d6252774ec39e96547f6f021b894
weskerwork@gmail.com <weskerwork@gmail.com>
2023-04-26 12:18:45 +0800

    Update low_level.py

--- src/low_level.py
+++ src/low_level.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import random
 from unitree_legged_msgs.msg import HighCmd, LowCmd
 
 def sit_down(cmd_publisher):
@@ -24,37 +25,41 @@ def stand_up(cmd_publisher):
     cmd_publisher.publish(cmd)
     rospy.sleep(2.5)
 
-def motor_control(low_cmd_publisher):
-    cmd = LowCmd()
-    cmd.mode = 0  # Position mode
+def motor_control(cmd_publisher, leg_index, duration):
+    cmd = HighCmd()
+    cmd.mode = 1  # Position mode
 
-    # Set desired motor positions, velocities, and torques
-    for i in range(12):
-        cmd.motor_cmd[i].q = 0  # Desired motor position
-        cmd.motor_cmd[i].dq = 0  # Desired motor velocity
-        cmd.motor_cmd[i].tau = 0  # Desired motor torque
+    # Set desired joint angles for the specified leg
+    target_position = [0, -0.8, 1.6] if leg_index % 2 == 0 else [0, 0.8, -1.6]
+    for i in range(3):
+        cmd.q1[leg_index * 3 + i] = target_position[i]
+    
+    cmd.duration = duration  # Time duration in milliseconds
 
-    cmd.motor_cmd[0].q = 0.5  # Example: Set desired position for motor 0
-
-    low_cmd_publisher.publish(cmd)
-    rospy.sleep(0.5)
+    cmd_publisher.publish(cmd)
+    rospy.sleep(duration / 1000.0 + 0.5)
 
 def control_sequence():
     rospy.init_node('control_sequence', anonymous=True)
 
-    high_cmd_publisher = rospy.Publisher('/unitree_go1/high_cmd', HighCmd, queue_size=1)
-    low_cmd_publisher = rospy.Publisher('/unitree_go1/low_cmd', LowCmd, queue_size=1)
+    cmd_publisher = rospy.Publisher('/unitree_go1/high_cmd', HighCmd, queue_size=1)
     rospy.sleep(0.5)
 
+    actions = [stand_up, sit_down, motor_control]
+    front_legs = [0, 2]  # Indices of the front legs
+
     while not rospy.is_shutdown():
-        stand_up(high_cmd_publisher)
-        rospy.sleep(30)
-        
-        sit_down(high_cmd_publisher)
-        rospy.sleep(30)
-        
-        motor_control(low_cmd_publisher)
-        rospy.sleep(30)
+        random.shuffle(actions)
+
+        for action in actions:
+            if action == motor_control:
+                leg_index = random.choice(front_legs)
+                duration = random.randint(2000, 4000)  # Random duration between 2 and 4 seconds
+                action(cmd_publisher, leg_index, duration)
+            else:
+                action(cmd_publisher)
+
+            rospy.sleep(30)
 
 if __name__ == '__main__':
     try:
